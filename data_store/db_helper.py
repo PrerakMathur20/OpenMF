8cf036ba182f76e0368a8afccbf6d494e94abcd7
Prerak Mathur <mathur.prerak@gmail.com>
2022-09-28 21:44:55 +0530

    FinalReport Added

--- data_store/db_helper.py
+++
@@ -1,9 +0,0 @@
-statements = {
-    'create_table': 'CREATE TABLE IF NOT EXISTS sessions (id INTEGER PRIMARY KEY AUTOINCREMENT, session_name TEXT,'
-                    'dbs_path TEXT, date TEXT)',
-
-}
-
-
-def insert_into_db():
-    pass
--- flask-backend/api/schema/case.py
+++
@@ -1,9 +0,0 @@
-"""
-Schema for Case Model.
-"""
-from api.extansions import ma
-
-
-class CaseSchema(ma.Schema):
-    class Meta:
-        fields = ('case_name', 'data_path', 'extracted_on', 'extractor_id', 'device_id')
